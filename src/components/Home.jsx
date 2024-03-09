515726d32fd4b71e778b11925f7ed710a0d30220
srikoti <ayushsinghsrikoti@gmail.com>
2024-03-09 21:28:28 +0530

    added loaders

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,38 @@
+import React, { useState, useEffect } from "react";
 import Navbar from "./navbar";
 import Banner from "./banner";
 import About from "./About";
 import Footer from "./Footer";
+import { ClipLoader } from "react-spinners";
 
-function Home(){
-    return(
+function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkImageLoading = () => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    };
+    checkImageLoading();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div className="flex justify-center items-center h-screen">
+          <ClipLoader color="black" loading={loading} size={150} />
+        </div>
+      ) : (
         <>
-            <Navbar/>
-            <Banner/>
-            <About/>
-            <Footer/>
+          <Navbar />
+          <Banner />
+          <About />
+          <Footer />
         </>
-    )
+      )}
+    </>
+  );
 }
 
 export default Home;
